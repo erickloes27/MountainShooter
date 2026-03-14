12c5555f10165543632a1789834fb7852f51cba5
erickloes27 <canaleric530@gmail.com>
2026-03-14 19:43:58 -0300

    Creating player shot Creating enemy shot

--- code/Const.py
+++ code/Const.py
@@ -15,9 +15,13 @@ ENTITY_SPEED = {
     'Level1Bg5': 2.5,
     'Level1Bg6': 3,
     'Player1': 3,
+    'Player1Shot': 1,
     'Player2': 3,
+    'Player2Shot': 3,
     'Enemy1': 2,
-    'Enemy2': 1
+    'Enemy1Shot': 5,
+    'Enemy2': 1,
+    'Enemy2Shot': 2,
 }
 ENTITY_HEALTH = {
     'Level1Bg0': 999,
@@ -28,11 +32,21 @@ ENTITY_HEALTH = {
     'Level1Bg5': 999,
     'Level1Bg6': 999,
     'Player1': 300,
+    'Player1Shot': 1,
     'Player2': 300,
+    'Player2Shot': 1,
     'Enemy1': 50,
-    'Enemy2': 60
+    'Enemy1Shot': 2,
+    'Enemy2': 60,
+    'Enemy2Shot': 2,
 }
 EVENT_ENEMY = pygame.USEREVENT + 1
+ENTITY_SHOT_DELAY = {
+    'Player1': 20,
+    'Player2': 15,
+    'Enemy1': 100,
+    'Enemy2': 200,
+}
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
